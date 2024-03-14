e1ee2c7b91e05f6a6222a41d2beab92ed3aae854
rim3250rim <hr3250c@gamil.com>
2024-03-14 16:56:15 +0900

    변경 내용 테스트

--- src/main/java/com/example/daybyday/entity/GuestEntity.java
+++ src/main/java/com/example/daybyday/entity/GuestEntity.java
@@ -1,6 +1,18 @@
 package com.example.daybyday.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@Table(name = "geust_table")
 public class GuestEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 
 }
